b3febdffecf53545ab40752a064fc08644804ea9
justin killam <killamjustin@gmail.com>
2022-02-15 21:24:57 -0800

    Adding branch and mips decoder

---
+++ MIPS_Decoder.py
@@ -0,0 +1,23 @@
+def File_Read(fileName):
+    fileObj=open(fileName,"r")
+    fileText=fileObj.read()
+    fileObj.close()
+    codeFormatted=[]
+    codeLines=fileText.split("\n")
+    for line in codeLines:
+        codeFormatted.append(line.split(" "))
+    return codeFormatted
+
+#lookups
+register_names=[]
+instruction_names=["lw","sw"]
+opcodes=[]
+function=[]
+
+#reading from a user specified input file
+inFile=input("Enter File extension:")
+inFile="C:\Users\killa\Documents\Python_Projects\New folder\CodeSample.txt"
+code=File_Read(inFile)
+
+for line in code:
+    arrayLoc=instruction_names.index(line[0])
